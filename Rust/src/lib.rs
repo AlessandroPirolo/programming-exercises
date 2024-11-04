a93d8d4c1a38bcf12b3e87051dcff62b6efa2703
AlessandroPirolo <Alessandropirolo@studenti.unipd.it>
2024-11-04 19:25:51 +0100

    Add test lib. Moved to Boxes from references with lifetime, add methods contains, is_empty, len, clear, back, front, push_front

---
+++ Rust/src/lib.rs
@@ -0,0 +1,13 @@
+mod linked_list;
+
+#[cfg(test)]
+mod tests {
+    use crate::linked_list::linked_list::LinkedList;
+
+    #[test]
+    fn list_creation() {
+        let _list : LinkedList<i32> = LinkedList::new();
+
+
+    }
+}
--- Rust/src/linked_list/mod.rs
+++ Rust/src/linked_list/mod.rs
@@ -1,31 +1,107 @@
 pub mod linked_list {
 
-    struct Node<'a, T> {
-        prev: Option<&'a Node<'a, T>>,
+    struct Node<T>
+    where
+        T: std::clone::Clone
+    {
+        prev: Option<Box<Node<T>>>,
         elem: T,
-        succ: Option<&'a Node<'a, T>>
+        succ: Option<Box<Node<T>>>
     }
 
-    pub struct LinkedList<'a, T> {
-        start : Option<Node<'a, T>>
+    pub struct LinkedList<T>
+    where
+        T: std::cmp::PartialOrd + std::clone::Clone
+    {
+        front : Option<Box<Node<T>>>,
+        back : Option<Box<Node<T>>>,
+        len : i32
     }
 
-    impl<'a, T> Node<'a, T> {
-        fn new(prev: Option<&'a Node<'a, T>>, elem: T, succ: Option<&'a Node<'a, T>>) -> Node<'a, T> {
+    impl<T> Node<T> 
+    where
+        T: std::cmp::PartialOrd + std::clone::Clone
+    {
+        fn new(prev: Option<Box<Node<T>>>, elem: T, succ: Option<Box<Node<T>>>) -> Node<T> {
             Node {
+                prev,
                 elem,
-                prev, 
                 succ
             }
         }
-
     }
 
-    impl<'a, T> LinkedList<'a, T> {
-        pub fn new() -> LinkedList<'a, T> {
-            LinkedList {
-                start: None 
+    impl<'a, T: std::clone::Clone> Clone for Node<T> {
+        fn clone(&self) -> Node<T> {
+            Node {
+                prev: self.prev.clone(),
+                elem: self.elem.clone(),
+                succ: self.succ.clone()
             }
+        }
+
+        fn clone_from(&mut self, source: &Node<T>) {
+            self.elem = source.elem.clone();
+            self.prev = source.prev.clone();
+            self.succ = source.succ.clone();
+        }
+    }
+
+    impl<T> LinkedList<T>
+    where
+        T: std::cmp::PartialOrd + std::clone::Clone
+    {
+        pub fn new() -> LinkedList<T> {
+            LinkedList {
+                front: None,
+                back: None,
+                len: 0
+            }
+        }
+
+        pub fn is_empty(&self) -> bool {
+            self.len == 0
+        }
+
+        pub fn len(&self) -> i32 {
+            self.len
+        }
+
+        pub fn clear(&mut self) {
+            self.front = None;
+            self.len = 0;
+        }
+
+        pub fn contains(&self, x: T) -> bool
+        where 
+            T: std::cmp::PartialOrd,
+        {
+            let mut cond = self.front.clone();
+            let mut find : bool = false;
+            while let Some(node) = cond {
+                find = node.elem == x;
+                cond = node.succ; 
+            }
+            find
+        }
+
+        pub fn front(&self) -> Option<&T> {
+            match &(self.front) {
+                None => None, 
+                Some(node) => Some(&(node.elem))
+            }
+        }
+
+        pub fn back(&self) -> Option<&T> {
+            match &(self.back) {
+                None => None, 
+                Some(node) => Some(&(node.elem))
+            }
+        }
+
+        pub fn push_front(&mut self, x: T) {
+            let node = Box::new(Node::new(None, x, self.front.clone()));
+            self.front = Some(node)
         }
 
     }
